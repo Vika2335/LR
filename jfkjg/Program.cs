a8f5e9882d235129068bf3f257832d5602e3ace5
Vika2335 <98107387+Vika2335@users.noreply.github.com>
2023-02-01 18:19:12 +0300

    Добавьте файлы проекта.

---
+++ jfkjg/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace jfkjg
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите a:");
+            double a = Convert.ToInt32(Console.ReadLine());
+            double x = Math.Sqrt((2 * a + Math.Sin(Math.Abs(3 * a))) / 3.56);
+            Console.Write("Введите x:");
+            double x1 = Convert.ToInt32(Console.ReadLine());
+            double y = Math.Sin((3.2 + Math.Sqrt(1 + x1)) / Math.Abs(5 * x1));
+            Console.WriteLine($"Функция x равна {x:0.000}");
+            Console.WriteLine($"Функция y равна {y:0.000}");
+        }
+    }
+}
+
