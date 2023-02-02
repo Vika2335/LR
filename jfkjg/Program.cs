f824222a4a4870092e9742b08dcf3249eb6e1b31
Vika2335 <98107387+Vika2335@users.noreply.github.com>
2023-02-02 10:11:45 +0300

    Update Program.cs

--- jfkjg/Program.cs
+++ jfkjg/Program.cs
@@ -5,10 +5,10 @@
         static void Main(string[] args)
         {
             Console.Write("Введите a:");
-            double a = Convert.ToDouble(Console.ReadLine());
+            double a = Math.Abs(double.Parse(Console.ReadLine()));
             double x = Math.Sqrt((2 * a + Math.Sin(Math.Abs(3 * a))) / 3.56);
             Console.Write("Введите x:");
-            double x1 = Convert.ToDouble(Console.ReadLine());
+            double x1 = Math.Abs(double.Parse(Console.ReadLine()));
             double y = Math.Sin((3.2 + Math.Sqrt(1 + x1)) / Math.Abs(5 * x1));
             Console.WriteLine($"Функция x равна {x:0.000}");
             Console.WriteLine($"Функция y равна {y:0.000}");
